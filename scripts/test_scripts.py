d416034ef22f1b329ad7013eb6f59e42b4b7aeff
ahmedWakil <raiyan33380@gmail.com>
2023-06-16 13:18:36 -0400

    update test scripts to handel '\r' character

--- scripts/test_scripts.py
+++ scripts/test_scripts.py
@@ -12,7 +12,7 @@ CHAR_TEST_LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
                      'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
                      'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 ALL_TEST_CATEGORY = ["Arabic", "English", "French", "German"]
-TEST_IGNORE = {'1', '/', '\xa0', '\n'}
+TEST_IGNORE = {'1', '/', '\xa0', '\n', '\r'}
 
 CATEGORY_DICT = {}
 for i, category in enumerate(ALL_TEST_CATEGORY):
